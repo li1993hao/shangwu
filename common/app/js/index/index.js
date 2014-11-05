0ff874a9c64adc06d1105480b2eba9052147d2b2
li1993hao <953445224@qq.com>
2014-11-05 12:55:26 +0800

    李浩

--- common/app/js/index/index.js
+++ common/app/js/index/index.js
@@ -2,7 +2,13 @@ var scrollTimer;
 var scrollWidth;
 var scrollCount;
 var scrollTime = 3000;
-
+function resetSize(){
+    clearInterval(scrollTimer);
+    $(".carousel_content div img").css("width", document.body.clientWidth+"px");
+    $(".carousel_content").css("width",scrollWidth*scrollCount+"px");
+    scrollWidth = document.body.clientWidth;
+    scrollTimer = setInterval(scrollCarousel,scrollTime);
+}
 $(function(){
     /**轮播控件***/
 
